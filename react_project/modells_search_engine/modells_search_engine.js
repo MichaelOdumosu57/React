3778c7b3763129ab64776830c050945f6e604e58
Michael Odumosu <michaelodumosu57@gmail.com>
2018-03-03 17:33:28 -0500

    Did not make too many commits because I wanted to understand react, react is just basically referring to everything you do in a certain way. when doing react its all typing and no thinking, and when you are trying to make it dyanmic its all thinking and not typing I belive, I have made great progress with this litle project

--- react_project/modells_search_engine/modells_search_engine.js
+++ react_project/modells_search_engine/modells_search_engine.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 
 
 
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
 
 
@@ -10,19 +18,37 @@ import ReactDOM from 'react-dom';
 class SearchBar extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {value: ''};
+    this.state = {search_item: '',checked: true};
 
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleBoxChange = this.handleBoxChange.bind(this);
+  }
+
+  handleTextChange(e){
+    this.props.onTextChange(e.target.value)
 
   }
+
+  handleBoxChange(e){
+    this.props.onHit(e.target.checked)
+  }
+
   render(){
+
+
+
+
     return (
       <div>
-        <input type="text" value={this.props.greeter}  />
-        <div>
-          <input type="checkbox"   />
-          <h6>{this.props.message}</h6>
-        </div>
+        <input type="text" placeholder={this.props.greeter}  onChange = {this.handleTextChange} value ={this.props.text_query} />
+        <p>
+          <input type="checkbox"  onChange = {this.handleBoxChange}  />
+          {' '}
+          {this.props.message}
+        </p>
+
       </div>
+
     );
   }
 }
@@ -32,25 +58,13 @@ class SearchBar extends React.Component{
 class Item extends React.Component{
   constructor(props) {
     super(props);
-    this.state = {color: ''};
+    this.state = {};
 
+    this.handleChange = this.handleChange.bind(this);
 
   }
 
 
-    label_red(){
-       () => this.setState({color:"red"})
-       return this.state.color;
-     }
-
-     label_cyan(){
-        () => this.setState({color:"cyan"})
-        return this.state.color;
-      }
-
-      label(sides){
-        () => this.setState({color})
-      }
 
 
   render(){
@@ -60,16 +74,9 @@ class Item extends React.Component{
         {category}
       </h6>
     );
-    let edges = null;
-    if(this.props.price == undefined){
-      return (<div color = {this.state.color}>{item_description}</div>);
-    }
-    else{
-      return ( <div color = {this.state.color}>{item_description}</div>);
-    }
 
 
-
+      return ( <div color = {this.props.color} onChange={this.handleChange}>{item_description}</div>);
 
 
 
@@ -77,28 +84,123 @@ class Item extends React.Component{
   }
 }
 
-function PriceBoard() {
-  return (
+class ProductCategoryRow extends React.Component {
+  render() {
+    const category = this.props.category;
+    return (
+      <tr>
+        <th colSpan="2">
+          {category}
+        </th>
+      </tr>
+    );
+  }
+}
 
-  <div>
-    <div color = "green">
-    <SearchBar
-      greeter="Search..."
-      message="Only Show Products In Stock" />
-      <h6>Name</h6>
-      <h6>Price</h6>
-      <Item name = "Sporting Goods" />
-      <Item name = "Football " price = "$49.99"/>
-      <Item name = "Football " price = "$49.99"/>
-      <Item name = "Football " price = "$49.99"/>
-      <Item name = "Electronics" />
-      <Item name = "iPod Touch" price = "$99.99"/>
-      <Item name = "iPod Touch" price = "$99.99"/>
-      <Item name = "iPod Touch" price = "$99.99"/>
+class ProductRow extends React.Component {
+  render() {
+    const product = this.props.product;
+    const name = product.stocked ?
+      product.name :
+      <span style={{color: 'red'}}>
+        {product.name}
+      </span>;
 
+    return (
+      <tr>
+        <td>{name}</td>
+        <td>{product.price}</td>
+      </tr>
+    );
+  }
+}
+
+class PriceBoard extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      text_query: '',
+      checked: true
+    }
+
+    this.handleTextChange = this.handleTextChange.bind(this);
+    this.handleCheckChange =  this.handleCheckChange.bind(this);
+
+  }
+
+  handleTextChange(name){
+    this.setState({
+      text_query: name
+    })
+  }
+
+  handleCheckChange(hit){
+    this.setState({
+      checked: hit
+    })
+  }
+
+
+  render(){
+
+    var rows = [];
+    let lastCategory = null;
+
+
+
+     PRODUCTS.forEach((product) => {
+       if (product.name.indexOf(this.state.text_query) === -1) {
+         return;
+       }
+       if (this.state.checked && !product.stocked) {
+         return;
+       }
+       if (product.category !== lastCategory) {
+         rows.push(
+           <ProductCategoryRow
+             category={product.category}
+             key={product.category} />
+         );
+       }
+       rows.push(
+         <ProductRow
+           product={product}
+           key={product.name}
+         />
+       );
+       lastCategory = product.category;
+    });
+
+
+    return (
+
+
+
+    <div>
+      <div color = "green">
+      <SearchBar
+        textChange = {this.state.text_query}
+        hit = {this.state.checked}
+        onTextChange = {this.handleTextChange}
+        onHit = {this.handleCheckChange }
+        greeter="Search..."
+        message="Only Show Products In Stock" />
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>{rows}</tbody>
+        </table>
+
+
+      </div>
     </div>
-  </div>
-  );
+    );
+  }s
 }
 
 
